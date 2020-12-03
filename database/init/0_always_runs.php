e8385a068765752a60f2276f8557c7b86e0dd631
Novum de Mozem <info@novum.nu>
2020-12-03 19:54:48 +0000

    Update 0_always_runs.php

--- database/init/0_always_runs.php
+++ database/init/0_always_runs.php
@@ -1,8 +1,4 @@
 <?php
-require_once '../../../../vendor/autoload.php';
-require_once '../../../../config/novum.svb/propel/config.php';
-require_once '../../../../config/novum.svb/config.php';
-
 $servicename = 'svb';
 $password = 'Mkwhwm!2020'; // Makkelijker kunnen we het wel maken!
 
